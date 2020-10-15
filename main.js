b1a4e84c22dcc37855289ca24b8abcd62aae988f
Olena <olenapetryshyn1@gmail.com>
2020-10-15 12:24:01 +0200

    DZ

---
+++ main.js
@@ -0,0 +1,39 @@
+// //1.
+// let marks = [80, 77, 68, 88, 95];
+// average = 0;
+
+// for (i = 0; i < marks.length;i++) {
+//   average = average + marks[i];
+//   averageMarks = average / marks.length;
+// }
+
+// console.log(`The average grade is ${averageMarks}`);
+
+// if (averageMarks < 60) {
+//     console.log('Corresponding grade: F');
+// } else if (averageMarks >= 60 && averageMarks < 70) {
+//     console.log('Corresponding grade: D'); 
+// } else if (averageMarks >= 70 && averageMarks < 80) {
+//     console.log('Corresponding grade: C');
+// } else if (averageMarks >= 80 && averageMarks < 90) {
+//     console.log('Corresponding grade: B');
+// } else if (averageMarks >= 90 && averageMarks <= 100) {
+//     console.log('Corresponding grade: A');
+// };
+
+// //2.
+// let sum = 0;
+
+// for (i = O; i < 1000; i++) {
+//   if (i % 3 === 0 || i % 5 === 0) {
+//     sum = sum + i;
+//   }
+// } 
+// console.log(sum);
+
+// //3.
+// for (x = 1; x <= 5; x++) {
+//   for (y = 1; y <= x; y++) {
+//     console.log(' * '.repeat(x, y));
+//   }
+// }
